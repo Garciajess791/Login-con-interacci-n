259ae44e79f4188fea02f343287e4887188fdd6c
Garciajess791 <116555191+Garciajess791@users.noreply.github.com>
2023-01-05 13:49:10 +0100

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html lang="ES-es">
 
 <head>
-    <title>EX_UF1846</title>
+    <title>Escoge Acción</title>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
